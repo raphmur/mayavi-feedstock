412707c2aa3ee77713ccc02a2276868e943492ce
Raphael Murray <raphael.murray@onera.fr>
2018-04-19 14:05:07 +0200

    update for pyface&traitsui v6

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,4 +4,4 @@ USERNAME=$(id -u -n)
 Xvfb :1 -screen 0 1280x1024x24 -auth localhost &
 export DISPLAY=:1
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
-killall -u $USERNAME Xvfb
+#killall -u $USERNAME Xvfb
